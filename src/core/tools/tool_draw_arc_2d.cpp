c28d98816242e48d93b301d36ccc3e7cffae276a
Lukas K. <lu@0x83.eu>
2023-10-30 22:37:41 +0100

    tool draw arc: support constraints

--- src/core/tools/tool_draw_arc_2d.cpp
+++ src/core/tools/tool_draw_arc_2d.cpp
@@ -5,6 +5,8 @@
 #include "document/constraint/constraint_points_coincident.hpp"
 #include "editor/editor_interface.hpp"
 #include "util/selection_util.hpp"
+#include "util/action_label.hpp"
+#include "util/glm_util.hpp"
 #include "tool_common_impl.hpp"
 
 namespace dune3d {
@@ -26,12 +28,39 @@ glm::dvec2 ToolDrawArc2D::get_cursor_pos_in_plane() const
     return m_wrkpl->project(m_intf.get_cursor_pos_for_plane(m_wrkpl->m_origin, m_wrkpl->get_normal()));
 }
 
+EntityAndPoint ToolDrawArc2D::get_entity_and_point(State state, bool flipped) const
+{
+    switch (state) {
+    case State::FROM:
+        if (!flipped)
+            return {m_temp_arc->m_uuid, 1};
+        else
+            return {m_temp_arc->m_uuid, 2};
+    case State::TO:
+        if (!flipped)
+            return {m_temp_arc->m_uuid, 2};
+        else
+            return {m_temp_arc->m_uuid, 1};
+    case State::CENTER:
+        return {m_temp_arc->m_uuid, 3};
+    }
+}
+
 ToolResponse ToolDrawArc2D::update(const ToolArgs &args)
 {
     if (args.type == ToolEventType::MOVE) {
         if (m_temp_arc) {
-            m_temp_arc->m_to = get_cursor_pos_in_plane();
-            m_temp_arc->m_center = (m_temp_arc->m_to + m_temp_arc->m_from) / 2.;
+            if (m_state == State::TO) {
+                if (!m_flipped)
+                    m_temp_arc->m_to = get_cursor_pos_in_plane();
+                else
+                    m_temp_arc->m_from = get_cursor_pos_in_plane();
+                m_temp_arc->m_center = (m_temp_arc->m_to + m_temp_arc->m_from) / 2.;
+            }
+            else { // CENTER
+                m_temp_arc->m_center =
+                        project_onto_perp_bisector(m_temp_arc->m_from, m_temp_arc->m_to, get_cursor_pos_in_plane());
+            }
         }
         update_tip();
         return ToolResponse();
@@ -39,66 +68,127 @@ ToolResponse ToolDrawArc2D::update(const ToolArgs &args)
     else if (args.type == ToolEventType::ACTION) {
         switch (args.action) {
         case InToolActionID::LMB: {
-            auto pte =
-                    entity_and_point_from_hover_selection(m_core.get_current_document(), m_intf.get_hover_selection());
-
-            bool had_arc = m_temp_arc;
-            if (!had_arc) {
+            switch (m_state) {
+            case State::FROM:
                 m_temp_arc = &add_entity<EntityArc2D>();
                 m_temp_arc->m_selection_invisible = true;
                 m_temp_arc->m_from = get_cursor_pos_in_plane();
-                m_temp_arc->m_to = get_cursor_pos_in_plane();
-                m_temp_arc->m_center = get_cursor_pos_in_plane();
+                m_temp_arc->m_to = get_cursor_pos_in_plane() + glm::dvec2(2, 0); // so that the solver is happy
+                m_temp_arc->m_center = get_cursor_pos_in_plane() + glm::dvec2(1, 0);
                 m_temp_arc->m_wrkpl = m_wrkpl->m_uuid;
-            }
+                if (m_constrain) {
+                    m_constraint_from = constrain_point(m_wrkpl->m_uuid, get_entity_and_point(m_state, m_flipped));
+                }
+                m_temp_arc->m_to = m_temp_arc->m_from;
+                m_temp_arc->m_center = m_temp_arc->m_from;
+                m_state = State::TO;
+                break;
 
+            case State::TO:
+                if (m_constrain) {
+                    m_constraint_from = constrain_point(m_wrkpl->m_uuid, get_entity_and_point(m_state, m_flipped));
+                }
+                m_state = State::CENTER;
+                break;
 
-            if (pte.has_value()) {
-                EntityArc2D *line = m_temp_arc;
-                unsigned int pt = 1;
-                if (had_arc)
-                    pt = 2;
-                auto pos = get_entity(pte->entity).get_point(pte->point, get_doc());
-
-                if (pt == 1)
-                    line->m_from = m_wrkpl->project(pos);
-                else
-                    line->m_to = m_wrkpl->project(pos);
-
-                auto &constraint = add_constraint<ConstraintPointsCoincident>();
-                constraint.m_entity1 = {line->m_uuid, pt};
-                constraint.m_entity2 = *pte;
-                constraint.m_wrkpl = m_wrkpl->m_uuid;
-            }
-
-            if (had_arc) {
+            case State::CENTER:
+                if (m_constrain) {
+                    constrain_point(m_wrkpl->m_uuid, get_entity_and_point(m_state, m_flipped));
+                }
                 return ToolResponse::commit();
             }
-        }
+        } break;
 
-        break;
+        case InToolActionID::TOGGLE_CONSTRUCTION: {
+            if (m_temp_arc)
+                m_temp_arc->m_construction = !m_temp_arc->m_construction;
+        } break;
+
+        case InToolActionID::TOGGLE_COINCIDENT_CONSTRAINT: {
+            m_constrain = !m_constrain;
+        } break;
+
+        case InToolActionID::FLIP_ARC: {
+            if (m_temp_arc) {
+                std::swap(m_temp_arc->m_from, m_temp_arc->m_to);
+                if (m_constraint_from) {
+                    m_constraint_from->replace_point(get_entity_and_point(State::FROM, m_flipped),
+                                                     get_entity_and_point(State::FROM, !m_flipped));
+                }
+                if (m_constraint_to) {
+                    m_constraint_to->replace_point(get_entity_and_point(State::TO, m_flipped),
+                                                   get_entity_and_point(State::TO, !m_flipped));
+                }
+                m_flipped = !m_flipped;
+            }
+        } break;
 
         case InToolActionID::RMB:
-
-
         case InToolActionID::CANCEL:
-
             return ToolResponse::revert();
 
         default:;
         }
     }
+    update_tip();
 
     return ToolResponse();
 }
 
 void ToolDrawArc2D::update_tip()
 {
-    auto pt = entity_and_point_from_hover_selection(get_doc(), m_intf.get_hover_selection());
-    if (pt.has_value())
-        m_intf.tool_bar_set_tool_tip("point");
+    std::vector<ActionLabelInfo> actions;
+
+    switch (m_state) {
+    case State::FROM:
+        actions.emplace_back(InToolActionID::LMB, "place from");
+        break;
+    case State::TO:
+        actions.emplace_back(InToolActionID::LMB, "place to");
+        break;
+    case State::CENTER:
+        actions.emplace_back(InToolActionID::LMB, "place center");
+        break;
+    }
+    actions.emplace_back(InToolActionID::RMB, "end tool");
+
+    switch (m_state) {
+    case State::FROM:
+        break;
+    case State::TO:
+    case State::CENTER:
+        actions.emplace_back(InToolActionID::FLIP_ARC, "flip arc");
+        break;
+    }
+
+    if (m_temp_arc) {
+        if (m_temp_arc->m_construction)
+            actions.emplace_back(InToolActionID::TOGGLE_CONSTRUCTION, "normal");
+        else
+            actions.emplace_back(InToolActionID::TOGGLE_CONSTRUCTION, "construction");
+    }
+
+    if (m_constrain)
+        actions.emplace_back(InToolActionID::TOGGLE_COINCIDENT_CONSTRAINT, "constraint off");
     else
-        m_intf.tool_bar_set_tool_tip("");
+        actions.emplace_back(InToolActionID::TOGGLE_COINCIDENT_CONSTRAINT, "constraint on");
+
+
+    m_intf.tool_bar_set_tool_tip("");
+    if (m_constrain) {
+        switch (m_state) {
+        case State::FROM:
+            set_constrain_tip("from");
+            break;
+        case State::TO:
+            set_constrain_tip("to");
+            break;
+        case State::CENTER:
+            set_constrain_tip("center");
+            break;
+        }
+    }
+    m_intf.tool_bar_set_actions(actions);
 }
 
 
--- src/core/tools/tool_draw_arc_2d.hpp
+++ src/core/tools/tool_draw_arc_2d.hpp
@@ -1,11 +1,12 @@
 #include "tool_common.hpp"
+#include "tool_helper_constrain.hpp"
 #include "in_tool_action/in_tool_action.hpp"
+#include "document/entity/entity_and_point.hpp"
 #include <optional>
 
 namespace dune3d {
 
-
-class ToolDrawArc2D : public ToolCommon {
+class ToolDrawArc2D : public virtual ToolCommon, public ToolHelperConstrain {
 public:
     using ToolCommon::ToolCommon;
 
@@ -15,9 +16,7 @@ public:
     {
         using I = InToolActionID;
         return {
-                I::LMB,
-                I::CANCEL,
-                I::RMB,
+                I::LMB, I::CANCEL, I::RMB, I::TOGGLE_COINCIDENT_CONSTRAINT, I::TOGGLE_CONSTRUCTION, I::FLIP_ARC,
         };
     }
 
@@ -27,7 +26,15 @@ public:
 private:
     class EntityArc2D *m_temp_arc = nullptr;
     const class EntityWorkplane *m_wrkpl = nullptr;
-    class ConstraintPointsCoincident *m_constraint = nullptr;
+    class Constraint *m_constraint_from = nullptr;
+    class Constraint *m_constraint_to = nullptr;
+
+    enum class State { FROM, TO, CENTER };
+    EntityAndPoint get_entity_and_point(State state, bool flipped) const;
+    State m_state = State::FROM;
+
+    bool m_constrain = true;
+    bool m_flipped = false;
 
     void update_tip();
 
