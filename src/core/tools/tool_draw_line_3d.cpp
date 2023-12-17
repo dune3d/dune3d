1db016b37e95eea19633777ed65165b65ab75205
Lukas K. <lu@0x83.eu>
2023-12-17 17:45:42 +0100

    tool draw line 3d: make it more similar to draw contour

--- src/core/tools/tool_draw_line_3d.cpp
+++ src/core/tools/tool_draw_line_3d.cpp
@@ -11,6 +11,7 @@ namespace dune3d {
 ToolResponse ToolDrawLine3D::begin(const ToolArgs &args)
 {
     update_tip();
+    m_intf.enable_hover_selection();
     return ToolResponse();
 }
 
@@ -19,13 +20,45 @@ void ToolDrawLine3D::update_tip()
     std::vector<ActionLabelInfo> actions;
     actions.reserve(9);
     actions.emplace_back(InToolActionID::LMB, "place point");
+    actions.emplace_back(InToolActionID::RMB, "end tool");
+
     if (m_temp_line) {
-        actions.emplace_back(InToolActionID::RMB, "finish current segment");
+        if (m_temp_line->m_construction)
+            actions.emplace_back(InToolActionID::TOGGLE_CONSTRUCTION, "normal");
+        else
+            actions.emplace_back(InToolActionID::TOGGLE_CONSTRUCTION, "construction");
+    }
+
+    if (m_constrain)
+        actions.emplace_back(InToolActionID::TOGGLE_COINCIDENT_CONSTRAINT, "coincident constraint off");
+    else
+        actions.emplace_back(InToolActionID::TOGGLE_COINCIDENT_CONSTRAINT, "coincident constraint on");
+
+    m_intf.tool_bar_set_actions(actions);
+
+    if (m_constrain) {
+        std::string what = m_temp_line ? "to" : "from";
+        set_constrain_tip(what);
     }
     else {
-        actions.emplace_back(InToolActionID::RMB, "end tool");
+        m_intf.tool_bar_set_tool_tip("");
     }
-    m_intf.tool_bar_set_actions(actions);
+}
+
+
+ToolResponse ToolDrawLine3D::end_tool()
+{
+    if (m_temp_line) {
+        m_core.get_current_document().m_entities.erase(m_temp_line->m_uuid);
+        m_temp_line = nullptr;
+        if (m_constraint)
+            m_core.get_current_document().m_constraints.erase(m_constraint->m_uuid);
+        return ToolResponse::commit();
+    }
+    else {
+        return ToolResponse::end();
+    }
+    return ToolResponse();
 }
 
 ToolResponse ToolDrawLine3D::update(const ToolArgs &args)
@@ -34,55 +67,72 @@ ToolResponse ToolDrawLine3D::update(const ToolArgs &args)
         if (m_temp_line) {
             m_temp_line->m_p2 = m_intf.get_cursor_pos();
         }
-        return ToolResponse();
     }
     else if (args.type == ToolEventType::ACTION) {
         switch (args.action) {
         case InToolActionID::LMB:
+        case InToolActionID::LMB_DOUBLE: {
+            if (m_constrain && m_temp_line) {
+                if (constrain_point(get_workplane_uuid(), {m_temp_line->m_uuid, 2})) {
+                    return ToolResponse::commit();
+                }
+            }
 
-        {
+            if (m_temp_line && m_last == m_intf.get_cursor_pos())
+                return end_tool();
+
             auto last_line = m_temp_line;
+
             m_temp_line = &add_entity<EntityLine3D>();
             m_temp_line->m_p1 = m_intf.get_cursor_pos();
             m_temp_line->m_p2 = m_intf.get_cursor_pos();
+            m_temp_line->m_selection_invisible = true;
+            m_temp_lines.push_back(m_temp_line);
             if (last_line) {
-                m_constraint = &add_constraint<ConstraintPointsCoincident>();
-                m_constraint->m_entity1 = {last_line->m_uuid, 2};
-                m_constraint->m_entity2 = {m_temp_line->m_uuid, 1};
+                auto &constraint = add_constraint<ConstraintPointsCoincident>();
+                constraint.m_entity1 = {last_line->m_uuid, 2};
+                constraint.m_entity2 = {m_temp_line->m_uuid, 1};
+                m_constraint = &constraint;
             }
-        }
-
-
             update_tip();
-            break;
+
+
+            if (m_constrain) {
+                if (!last_line) {
+                    if (auto constraint = constrain_point(get_workplane_uuid(), {m_temp_line->m_uuid, 1})) {
+                        m_constraint = constraint;
+                    }
+                }
+            }
+
+            m_last = m_intf.get_cursor_pos();
+        } break;
+
+
+        case InToolActionID::TOGGLE_CONSTRUCTION: {
+            if (m_temp_line) {
+                m_temp_line->m_construction = !m_temp_line->m_construction;
+                for (auto en : m_temp_lines) {
+                    en->m_construction = m_temp_line->m_construction;
+                }
+            }
+        } break;
+
+        case InToolActionID::TOGGLE_COINCIDENT_CONSTRAINT: {
+            m_constrain = !m_constrain;
+        } break;
 
         case InToolActionID::RMB:
-            if (m_temp_line) {
-                m_core.get_current_document().m_entities.erase(m_temp_line->m_uuid);
-                m_temp_line = nullptr;
-                if (m_constraint)
-                    m_core.get_current_document().m_constraints.erase(m_constraint->m_uuid);
-            }
-            else {
-
-                return ToolResponse::commit();
-            }
-            break;
-
-
-        case InToolActionID::CANCEL:
-            if (m_temp_line) {
-                m_core.get_current_document().m_entities.erase(m_temp_line->m_uuid);
-                m_temp_line = nullptr;
-                if (m_constraint)
-                    m_core.get_current_document().m_constraints.erase(m_constraint->m_uuid);
-            }
-            return ToolResponse::revert();
+        case InToolActionID::CANCEL: {
+            auto r = end_tool();
+            if (r.result != ToolResponse::Result::NOP)
+                return r;
+        } break;
 
         default:;
         }
     }
-
+    update_tip();
     return ToolResponse();
 }
 } // namespace dune3d
--- src/core/tools/tool_draw_line_3d.hpp
+++ src/core/tools/tool_draw_line_3d.hpp
@@ -1,10 +1,11 @@
 #include "tool_common.hpp"
 #include "in_tool_action/in_tool_action.hpp"
+#include "tool_helper_constrain.hpp"
 
 namespace dune3d {
 
 
-class ToolDrawLine3D : public ToolCommon {
+class ToolDrawLine3D : public virtual ToolCommon, public ToolHelperConstrain {
 public:
     using ToolCommon::ToolCommon;
 
@@ -14,15 +15,19 @@ public:
     {
         using I = InToolActionID;
         return {
-                I::LMB,
-                I::CANCEL,
-                I::RMB,
+                I::LMB, I::LMB_DOUBLE, I::CANCEL, I::RMB, I::TOGGLE_CONSTRUCTION, I::TOGGLE_COINCIDENT_CONSTRAINT,
         };
     }
 
 private:
     class EntityLine3D *m_temp_line = nullptr;
-    class ConstraintPointsCoincident *m_constraint = nullptr;
+    std::vector<EntityLine3D *> m_temp_lines;
+    class Constraint *m_constraint = nullptr;
+    bool m_constrain = true;
+    ToolResponse end_tool();
+
+    glm::dvec3 m_last;
+
 
     void update_tip();
 };
