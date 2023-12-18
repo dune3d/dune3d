41c51f308c5a550209a6a7acc88891d9aa23f300
Lukas K. <lu@0x83.eu>
2023-12-18 12:59:49 +0100

    ToolHelperConstrain: factor our helpers

--- src/core/tools/tool_helper_constrain.cpp
+++ src/core/tools/tool_helper_constrain.cpp
@@ -8,64 +8,89 @@
 #include "tool_common_impl.hpp"
 
 namespace dune3d {
+
+Constraint *ToolHelperConstrain::constrain_point(Constraint::Type type, const UUID &wrkpl, const EntityAndPoint &enp,
+                                                 const EntityAndPoint &enp_to_constrain)
+{
+    switch (type) {
+    case Constraint::Type::POINTS_COINCIDENT: {
+        auto &constraint = add_constraint<ConstraintPointsCoincident>();
+        constraint.m_entity1 = enp;
+        constraint.m_entity2 = enp_to_constrain;
+        constraint.m_wrkpl = wrkpl;
+        m_core.solve_current();
+        return &constraint;
+    }
+    case Constraint::Type::POINT_ON_LINE: {
+        auto &constraint = add_constraint<ConstraintPointOnLine>();
+        constraint.m_line = enp.entity;
+        constraint.m_point = enp_to_constrain;
+        constraint.m_wrkpl = wrkpl;
+        constraint.m_modify_to_satisfy = true;
+        m_core.solve_current();
+        return &constraint;
+    }
+    case Constraint::Type::POINT_ON_CIRCLE: {
+        auto &constraint = add_constraint<ConstraintPointOnCircle>();
+        constraint.m_circle = enp.entity;
+        constraint.m_point = enp_to_constrain;
+        constraint.m_modify_to_satisfy = true;
+        m_core.solve_current();
+        return &constraint;
+    }
+    default:
+        return nullptr;
+    }
+}
+
 Constraint *ToolHelperConstrain::constrain_point(const UUID &wrkpl, const EntityAndPoint &enp_to_constrain)
+{
+    const auto ct = get_constraint_type();
+    if (!ct)
+        return nullptr;
+    auto hsel = m_intf.get_hover_selection();
+    const auto enp = hsel->get_entity_and_point();
+
+    return constrain_point(*ct, wrkpl, enp, enp_to_constrain);
+}
+
+std::optional<Constraint::Type> ToolHelperConstrain::get_constraint_type()
 {
     if (auto hsel = m_intf.get_hover_selection()) {
         if (hsel->type == SelectableRef::Type::ENTITY) {
             const auto enp = hsel->get_entity_and_point();
             if (get_doc().is_valid_point(enp)) {
-                auto &constraint = add_constraint<ConstraintPointsCoincident>();
-                constraint.m_entity1 = enp;
-                constraint.m_entity2 = enp_to_constrain;
-                constraint.m_wrkpl = wrkpl;
-                m_core.solve_current();
-                return &constraint;
+                return Constraint::Type::POINTS_COINCIDENT;
             }
             else if (enp.point == 0) {
                 auto &entity = get_entity(enp.entity);
                 if (entity.get_type() == Entity::Type::LINE_2D || entity.get_type() == Entity::Type::LINE_3D) {
-                    auto &constraint = add_constraint<ConstraintPointOnLine>();
-                    constraint.m_line = entity.m_uuid;
-                    constraint.m_point = enp_to_constrain;
-                    constraint.m_wrkpl = wrkpl;
-                    constraint.m_modify_to_satisfy = true;
-                    m_core.solve_current();
-                    return &constraint;
+                    return Constraint::Type::POINT_ON_LINE;
                 }
                 else if (entity.get_type() == Entity::Type::ARC_2D || entity.get_type() == Entity::Type::CIRCLE_2D) {
-                    auto &constraint = add_constraint<ConstraintPointOnCircle>();
-                    constraint.m_circle = entity.m_uuid;
-                    constraint.m_point = enp_to_constrain;
-                    constraint.m_modify_to_satisfy = true;
-                    m_core.solve_current();
-                    return &constraint;
+                    return Constraint::Type::POINT_ON_CIRCLE;
                 }
             }
         }
     }
-    return nullptr;
+    return {};
 }
 
 std::string ToolHelperConstrain::get_constrain_tip(const std::string &what)
 {
-    if (auto hsel = m_intf.get_hover_selection()) {
-        if (hsel->type == SelectableRef::Type::ENTITY) {
-            const auto enp = hsel->get_entity_and_point();
-            if (get_doc().is_valid_point(enp)) {
-                return "constrain " + what + " on point";
-            }
-            else if (enp.point == 0) {
-                auto &entity = get_entity(enp.entity);
-                if (entity.get_type() == Entity::Type::LINE_2D || entity.get_type() == Entity::Type::LINE_3D) {
-                    return "constrain " + what + " on line";
-                }
-                else if (entity.get_type() == Entity::Type::ARC_2D || entity.get_type() == Entity::Type::CIRCLE_2D) {
-                    return "constrain " + what + " on circle";
-                }
-            }
-        }
+    const auto ct = get_constraint_type();
+    if (!ct)
+        return "";
+    switch (*ct) {
+    case Constraint::Type::POINTS_COINCIDENT:
+        return "constrain " + what + " on point";
+    case Constraint::Type::POINT_ON_LINE:
+        return "constrain " + what + " on line";
+    case Constraint::Type::POINT_ON_CIRCLE:
+        return "constrain " + what + " on circle";
+    default:
+        return "";
     }
-    return "";
 }
 
 
--- src/core/tools/tool_helper_constrain.hpp
+++ src/core/tools/tool_helper_constrain.hpp
@@ -1,14 +1,19 @@
 #pragma once
 #include "tool_common.hpp"
+#include "document/constraint/constraint.hpp"
+#include <optional>
 
 namespace dune3d {
 
-class Constraint;
 class EntityAndPoint;
 
 class ToolHelperConstrain : public virtual ToolCommon {
 protected:
-    Constraint *constrain_point(const UUID &wrkpl, const EntityAndPoint &enp);
+    Constraint *constrain_point(const UUID &wrkpl, const EntityAndPoint &enp_to_constrain);
+    Constraint *constrain_point(Constraint::Type type, const UUID &wrkpl, const EntityAndPoint &enp,
+                                const EntityAndPoint &enp_to_constrain);
+    std::optional<Constraint::Type> get_constraint_type();
+
     void set_constrain_tip(const std::string &what);
     std::string get_constrain_tip(const std::string &what);
 };
