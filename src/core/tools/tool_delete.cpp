6c527769872fd8bfd3249aee75e390f6ca0ef6ea
Lukas K. <lu@0x83.eu>
2024-01-02 12:27:28 +0100

    fix deleting anchors referenced by constraints

--- src/core/tools/tool_delete.cpp
+++ src/core/tools/tool_delete.cpp
@@ -5,6 +5,7 @@
 #include "document/group/group.hpp"
 #include "tool_common_impl.hpp"
 #include <iostream>
+#include <algorithm>
 
 namespace dune3d {
 
@@ -28,6 +29,8 @@ ToolResponse ToolDelete::begin(const ToolArgs &args)
     auto &doc = get_doc();
 
     Document::ItemsToDelete items_to_delete;
+    const Group *first_anchor_group = nullptr;
+    std::set<EntityAndPoint> deleted_anchors;
 
     for (auto &sr : m_selection) {
         if (sr.type == SelectableRef::Type::ENTITY) {
@@ -35,10 +38,14 @@ ToolResponse ToolDelete::begin(const ToolArgs &args)
             if (!en.can_delete(doc))
                 continue;
             if (auto en_step = dynamic_cast<EntitySTEP *>(&en)) {
-                if (en_step->m_anchors.contains(sr.point))
+                if (en_step->m_anchors.contains(sr.point)) {
                     en_step->remove_anchor(sr.point);
-                else
+                    deleted_anchors.insert(sr.get_entity_and_point());
+                    doc.accumulate_first_group(first_anchor_group, en_step->m_group);
+                }
+                else {
                     items_to_delete.entities.insert(sr.item);
+                }
             }
             else {
                 items_to_delete.entities.insert(sr.item);
@@ -46,6 +53,14 @@ ToolResponse ToolDelete::begin(const ToolArgs &args)
         }
         else if (sr.type == SelectableRef::Type::CONSTRAINT)
             items_to_delete.constraints.insert(sr.item);
+    }
+
+    for (const auto &[uu, constr] : doc.m_constraints) {
+        std::set<EntityAndPoint> isect;
+        std::ranges::set_intersection(constr->get_referenced_entities_and_points(), deleted_anchors,
+                                      std::inserter(isect, isect.begin()));
+        if (isect.size())
+            items_to_delete.constraints.insert(uu);
     }
 
     auto extra_items = doc.get_additional_items_to_delete(items_to_delete);
@@ -59,7 +74,13 @@ ToolResponse ToolDelete::begin(const ToolArgs &args)
         std::cout << "delete constraint " << (std::string)uu << std::endl;
     }
     items_to_delete.append(extra_items);
+
     doc.delete_items(items_to_delete);
+
+    if (first_anchor_group) {
+        doc.set_group_solve_pending(first_anchor_group->m_uuid);
+    }
+
 
     return ToolResponse::commit();
 }
