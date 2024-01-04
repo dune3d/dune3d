e50429cc4a88a07d6f72629f52691f4a88d1b411
Koen Schmeets <hello@koenschmeets.nl>
2024-01-05 00:13:03 +0100

    Cleanup, make cmake required on non-darwin platforms

--- scripts/build_macos.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# use the cross file based on the architecture, we assume homebrew installs packages in /usr/local on x86_64 and /opt/homebrew on arm64
-meson setup --cross-file macos-$(uname -m).ini build
-
-meson compile -C build
