d14364307d8c6dab23c61318ddb92b3e32c6235a
Lukas K. <lu@0x83.eu>
2024-10-17 00:16:46 +0200

    check_version: check release URL

--- check_version.py
+++ check_version.py
@@ -4,13 +4,18 @@ import version as v
 import xml.etree.ElementTree as ET
 tree = ET.parse('org.dune3d.dune3d.metainfo.xml')
 root = tree.getroot()
-version_from_xml = root.find("releases").find("release").attrib["version"]
+release = root.find("releases").find("release")
+version_from_xml = release.attrib["version"]
 
 rc = 0
 if version_from_xml == v.string :
 	print("Version okay")
 else:
 	print("Version mismatch %s != %s"%(v.string, version_from_xml))
+	rc = 1
+
+if release.find("url").text.strip() != f"https://github.com/dune3d/dune3d/releases/tag/v{v.string}" :
+	print("Release URL mismatch")
 	rc = 1
 
 #Check changelog versions
