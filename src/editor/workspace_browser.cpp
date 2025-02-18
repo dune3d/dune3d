dune3d/dune3d

commit 95a2cd7e50d082d49d24d017ea4fa86776cf0469

workspace browser: store body expanded/not expanded in document view
changelog: Enhancements/Editor: don't forget collapsed state of bodies
in workspace browser

src/editor/workspace_browser.cpp
@@ -66,6 +66,7 @@ public:
     UUID m_uuid;
     UUID m_doc;
     Glib::Property<bool> m_check_active;
+    Glib::Property<bool> m_expanded;
     Glib::Property<bool> m_check_sensitive;
     Glib::Property<bool> m_solid_model_active;
     Glib::Property<bool> m_has_color;
@@ -87,8 +88,9 @@ public:
 private:
     BodyItem()
         : Glib::ObjectBase("BodyItem"), m_name(*this, "name"), m_check_active(*this, "check_active", false),
-          m_check_sensitive(*this, "check_sensitive", true), m_solid_model_active(*this, "m_solid_model_active", true),
-          m_has_color(*this, "has_color", false), m_color(*this, "color")
+          m_expanded(*this, "expanded", true), m_check_sensitive(*this, "check_sensitive", true),
+          m_solid_model_active(*this, "m_solid_model_active", true), m_has_color(*this, "has_color", false),
+          m_color(*this, "color")
     {
         m_group_store = Gio::ListStore<GroupItem>::create();
     }
@@ -195,6 +197,7 @@ void WorkspaceBrowser::block_signals()
     m_signal_body_checked.block();
     m_signal_group_selected.block();
     m_signal_body_solid_model_checked.block();
+    m_signal_body_expanded.block();
 }
 
 void WorkspaceBrowser::unblock_signals()
@@ -204,6 +207,7 @@ void WorkspaceBrowser::unblock_signals()
     m_signal_body_checked.unblock();
     m_signal_group_selected.unblock();
     m_signal_body_solid_model_checked.unblock();
+    m_signal_body_expanded.unblock();
 }
 
 static std::string icon_name_from_status(GroupStatusMessage::Status st)
@@ -258,14 +262,15 @@ void WorkspaceBrowser::update_current_group(const std::map<UUID, DocumentView> &
         bool after_active = false;
         for (size_t i_body = 0; i_body < it_doc.m_body_store->get_n_items(); i_body++) {
             auto &it_body = *it_doc.m_body_store->get_item(i_body);
-
+            const bool is_current_body = body_uu == it_body.m_uuid && is_current_doc;
             it_body.m_check_sensitive = (body_uu != it_body.m_uuid) || !is_current_doc;
-            if (body_uu == it_body.m_uuid && is_current_doc)
+            if (is_current_body)
                 it_body.m_check_active = true;
             else
                 it_body.m_check_active = doc_view.body_is_visible(it_body.m_uuid);
 
             it_body.m_solid_model_active = doc_view.body_solid_model_is_visible(it_body.m_uuid);
+            it_body.m_expanded = doc_view.body_is_expanded(it_body.m_uuid) | is_current_body;
 
 
             for (size_t i_group = 0; i_group < it_body.m_group_store->get_n_items(); i_group++) {
@@ -544,6 +549,8 @@ public:
         m_connections.push_back(
                 it.m_active.get_proxy().signal_changed().connect([this, &it] { update_label_attrs(it); }));
 
+        set_hide_expander(true);
+
         m_browser.unblock_signals();
     }
     void bind(BodyItem &it)
@@ -569,6 +576,9 @@ public:
         m_bindings.push_back(Glib::Binding::bind_property_value(it.m_solid_model_active.get_proxy(),
                                                                 m_solid_toggle->property_active(),
                                                                 Glib::Binding::Flags::SYNC_CREATE));
+        get_list_row()->set_expanded(it.m_expanded);
+        m_connections.push_back(it.m_expanded.get_proxy().signal_changed().connect(
+                [this, &it] { get_list_row()->set_expanded(it.m_expanded); }));
         const bool has_color = it.m_has_color.get_value();
         if (has_color)
             m_solid_toggle->set_body_color(it.m_color.get_value());
@@ -579,6 +589,15 @@ public:
                 it.m_color.get_proxy().signal_changed().connect([this, &it] { update_solid_color(it); }));
         m_connections.push_back(
                 it.m_has_color.get_proxy().signal_changed().connect([this, &it] { update_solid_color(it); }));
+
+        const auto body_uu = it.m_uuid;
+        m_connections.push_back(get_list_row()->property_expanded().signal_changed().connect([this, body_uu, &it] {
+            const auto expanded = get_list_row()->get_expanded();
+            if (m_browser.emit_body_expanded(body_uu, expanded))
+                it.m_expanded = expanded;
+            else
+                it.m_expanded = true;
+        }));
 
         m_browser.unblock_signals();
     }
@@ -629,6 +648,7 @@ public:
         m_group = nullptr;
         m_body = nullptr;
         m_doc = nullptr;
+        set_hide_expander(false);
     }
 
 private:
@@ -937,6 +957,11 @@ void WorkspaceBrowser::emit_add_group(GroupType type, AddGroupMode add_group_mod
     m_signal_add_group.emit(type, add_group_mode);
 }
 
+bool WorkspaceBrowser::emit_body_expanded(const UUID &body_uu, bool expanded)
+{
+    return m_signal_body_expanded.emit(body_uu, expanded);
+}
+
 Glib::RefPtr<Gio::ListModel> WorkspaceBrowser::create_model(const Glib::RefPtr<Glib::ObjectBase> &item)
 {
     // The items in a StringList are StringObjects.
@@ -971,6 +996,24 @@ void WorkspaceBrowser::group_prev_next(int dir)
 
 void WorkspaceBrowser::select_group(const UUID &uu)
 {
+    {
+        const auto n = m_selection_model->get_n_items();
+
+        const auto body_group =
+                m_core.get_current_document().get_group(uu).find_body(m_core.get_current_document()).group.m_uuid;
+        {
+            for (size_t i = 0; i < n; i++) {
+                auto row = std::dynamic_pointer_cast<Gtk::TreeListRow>(m_selection_model->get_object(i));
+                if (!row)
+                    continue;
+                auto it = std::dynamic_pointer_cast<BodyItem>(row->get_item());
+                if (!it)
+                    continue;
+                if (it->m_uuid == body_group)
+                    row->set_expanded(true);
+            }
+        }
+    }
     const auto n = m_selection_model->get_n_items();
 
     for (size_t i = 0; i < n; i++) {

src/editor/workspace_browser.hpp
@@ -96,6 +96,12 @@ public:
         return m_signal_activate_link;
     }
 
+    using type_signal_item_expanded = sigc::signal<bool(UUID, bool)>;
+    type_signal_item_expanded signal_body_expanded()
+    {
+        return m_signal_body_expanded;
+    }
+
 
     void group_prev_next(int dir);
     void select_group(const UUID &uu);
@@ -140,7 +146,10 @@ private:
     type_signal_group_selected m_signal_set_body_color;
     type_signal_group_selected m_signal_reset_body_color;
 
+    type_signal_item_expanded m_signal_body_expanded;
+
     void emit_add_group(GroupType type, AddGroupMode add_group_mode = AddGroupMode::WITHOUT_BODY);
+    bool emit_body_expanded(const UUID &body_uu, bool expanded);
 
     void block_signals();
     void unblock_signals();

src/editor/editor_workspace_browser.cpp
@@ -46,6 +46,18 @@ void Editor::init_workspace_browser()
             sigc::mem_fun(*this, &Editor::on_workspace_browser_reset_body_color));
     m_workspace_browser->signal_set_body_color().connect(
             sigc::mem_fun(*this, &Editor::on_workspace_browser_set_body_color));
+    m_workspace_browser->signal_body_expanded().connect([this](const UUID &body_uu, bool expanded) {
+        if (m_core.get_current_document()
+                            .get_group(m_core.get_current_group())
+                            .find_body(m_core.get_current_document())
+                            .group.m_uuid
+                    == body_uu
+            && expanded == false) {
+            return false;
+        }
+        get_current_document_view().m_body_views[body_uu].m_expanded = expanded;
+        return true;
+    });
 
     m_workspace_browser->set_sensitive(m_core.has_documents());
 

src/workspace/document_view.cpp
@@ -23,6 +23,13 @@ bool DocumentView::body_is_visible(const UUID &uu) const
     return true;
 }
 
+bool DocumentView::body_is_expanded(const UUID &uu) const
+{
+    if (m_body_views.contains(uu))
+        return m_body_views.at(uu).m_expanded;
+    return true;
+}
+
 bool DocumentView::body_solid_model_is_visible(const UUID &uu) const
 {
     if (m_body_views.contains(uu))
@@ -70,13 +77,14 @@ json DocumentView::GroupView::serialize() const
 
 DocumentView::BodyView::BodyView() = default;
 DocumentView::BodyView::BodyView(const json &j)
-    : m_visible(j.at("visible").get<bool>()), m_solid_model_visible(j.at("solid_model_visible").get<bool>())
+    : m_visible(j.at("visible").get<bool>()), m_solid_model_visible(j.at("solid_model_visible").get<bool>()),
+      m_expanded(j.value("expanded", true))
 {
 }
 
 json DocumentView::BodyView::serialize() const
 {
-    return {{"visible", m_visible}, {"solid_model_visible", m_solid_model_visible}};
+    return {{"visible", m_visible}, {"solid_model_visible", m_solid_model_visible}, {"expanded", m_expanded}};
 }
 
 json DocumentView::serialize() const

src/workspace/document_view.hpp
@@ -32,6 +32,7 @@ public:
         BodyView(const json &j);
         bool m_visible = true;
         bool m_solid_model_visible = true;
+        bool m_expanded = true;
         json serialize() const;
     };
     std::map<UUID, BodyView> m_body_views;
@@ -39,6 +40,7 @@ public:
     std::map<UUID, std::unique_ptr<EntityView>> m_entity_views;
 
     bool body_is_visible(const UUID &uu) const override;
+    bool body_is_expanded(const UUID &uu) const;
     bool body_solid_model_is_visible(const UUID &uu) const override;
     bool group_is_visible(const UUID &uu) const override;
 

commit 09b14b12e0ebf674a45d8f771439443ba0384b3e

don't throw exception when opening more than one document
got broken in 95a2cd7e50d082d49d24d017ea4fa86776cf0469

fixes #154

src/editor/workspace_browser.cpp
@@ -317,7 +317,7 @@ void WorkspaceBrowser::update_current_group(const std::map<UUID, DocumentView> &
                     after_active = true;
             }
         }
-        select_group(doci.get_current_group());
+        select_group(doci.get_uuid(), doci.get_current_group());
     }
     if (m_core.has_documents()) {
         auto &current_group = m_core.get_current_document().get_group(m_core.get_current_group());
@@ -1007,11 +1007,16 @@ void WorkspaceBrowser::group_prev_next(int dir)
 
 void WorkspaceBrowser::select_group(const UUID &uu)
 {
+    select_group(m_core.get_current_idocument_info().get_uuid(), uu);
+}
+
+void WorkspaceBrowser::select_group(const UUID &doc_uu, const UUID &uu)
+{
     {
         const auto n = m_selection_model->get_n_items();
 
-        const auto body_group =
-                m_core.get_current_document().get_group(uu).find_body(m_core.get_current_document()).group.m_uuid;
+        auto &doc = m_core.get_idocument_info(doc_uu).get_document();
+        const auto body_group = doc.get_group(uu).find_body(doc).group.m_uuid;
         {
             for (size_t i = 0; i < n; i++) {
                 auto row = std::dynamic_pointer_cast<Gtk::TreeListRow>(m_selection_model->get_object(i));
@@ -1020,7 +1025,7 @@ void WorkspaceBrowser::select_group(const UUID &uu)
                 auto it = std::dynamic_pointer_cast<BodyItem>(row->get_item());
                 if (!it)
                     continue;
-                if (it->m_uuid == body_group)
+                if (it->m_doc == doc_uu && it->m_uuid == body_group)
                     row->set_expanded(true);
             }
         }

src/editor/workspace_browser.hpp
@@ -155,6 +155,9 @@ private:
     void unblock_signals();
     unsigned int m_blocked_count = 0;
 
+    void select_group(const UUID &doc_uu, const UUID &uu);
+
+
     static void update_name(DocumentItem &it, IDocumentInfo &doci);
 
     sigc::connection m_toast_connection;