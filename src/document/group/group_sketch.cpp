dune3d/dune3d

commit 7237eb81991c10b702ff4e86b69fb99ec3a25075

sketch group: save solid model operation
changelog: Bugfixes/Editor: persist solid model operation in sketch groups

src/document/group/group_sketch.cpp
@@ -7,13 +7,16 @@ GroupSketch::GroupSketch(const UUID &uu) : Group(uu)
 {
 }
 
-GroupSketch::GroupSketch(const UUID &uu, const json &j) : Group(uu, j)
+GroupSketch::GroupSketch(const UUID &uu, const json &j)
+    : Group(uu, j), m_operation(j.value("operation", Operation::UNION))
 {
 }
 
 json GroupSketch::serialize() const
 {
-    return Group::serialize();
+    auto j = Group::serialize();
+    j["operation"] = m_operation;
+    return j;
 }
 
 std::unique_ptr<Group> GroupSketch::clone() const

scripts/app_versions.yml
@@ -30,3 +30,4 @@ versions:
     28: add clone group
     29: add pipe group
     30: add picture entities
+    31: save solid model operation in sketch groups

src/document/document.cpp
@@ -52,7 +52,7 @@ json Document::serialize() const
     return j;
 }
 
-static const unsigned int app_version = 30;
+static const unsigned int app_version = 31;
 
 unsigned int Document::get_app_version()
 {

commit 949ebd5c0eea44101f2769de748da7d3dff61529

correctly load sketch groups without solid model operation
also save operation as string, not as int

got broken in 7237eb81991c10b702ff4e86b69fb99ec3a25075

src/document/group/group_sketch.cpp
@@ -1,6 +1,7 @@
 #include "group_sketch.hpp"
 #include "nlohmann/json.hpp"
 #include "document/solid_model.hpp"
+#include "igroup_solid_model_json.hpp"
 
 namespace dune3d {
 GroupSketch::GroupSketch(const UUID &uu) : Group(uu)