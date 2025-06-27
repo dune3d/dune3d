b9d94f94d0af5595dd9ec19350f4d83e9d0f6996
Lukas K. <lu@0x83.eu>
2025-06-27 22:43:16 +0200

    pictures: use Gdk::Texture rather than pixbuf

--- src/util/picture_util.cpp
+++ src/util/picture_util.cpp
@@ -1,34 +1,30 @@
 #include "picture_util.hpp"
 #include "picture_data.hpp"
 #include "fs_util.hpp"
-#include <gdkmm/pixbuf.h>
+#include <gdkmm/texture.h>
 
 namespace dune3d {
 std::shared_ptr<const PictureData> picture_data_from_file(const std::filesystem::path &filename)
 {
-    auto pixbuf = Gdk::Pixbuf::create_from_file(path_to_string(filename));
-    return picture_data_from_pixbuf(pixbuf);
+    auto tex = Gdk::Texture::create_from_filename(path_to_string(filename));
+    return picture_data_from_texture(tex);
 }
 
-std::shared_ptr<const class PictureData> picture_data_from_pixbuf(Glib::RefPtr<Gdk::Pixbuf> pixbuf)
+std::shared_ptr<const class PictureData> picture_data_from_texture(Glib::RefPtr<Gdk::Texture> texture)
 {
     std::vector<uint32_t> v;
-    auto w = pixbuf->get_width();
-    auto h = pixbuf->get_height();
+    auto w = texture->get_width();
+    auto h = texture->get_height();
     v.resize(w * h);
-    const uint8_t *px = pixbuf->get_pixels();
-    const auto nch = pixbuf->get_n_channels();
+    std::vector<guchar> buf(w * h * 4);
+    auto px = buf.data();
+    texture->download(px, w * 4);
     for (int y = 0; y < h; y++) {
         for (int x = 0; x < w; x++) {
-            auto p = px + (x * nch);
-            if (nch == 3) {
-                v[y * w + x] = (p[0]) | (p[1] << 8) | (p[2] << 16) | (0xff << 24);
-            }
-            else if (nch == 4 && pixbuf->get_has_alpha()) {
-                v[y * w + x] = (p[0]) | (p[1] << 8) | (p[2] << 16) | (p[3] << 24);
-            }
+            auto p = px + (x * 4);
+            v[y * w + x] = (p[2]) | (p[1] << 8) | (p[0] << 16) | (p[3] << 24);
         }
-        px += pixbuf->get_rowstride();
+        px += w * 4;
     }
     return std::make_shared<PictureData>(UUID::random(), w, h, std::move(v));
 }
