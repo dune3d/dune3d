dune3d/dune3d

commit 7880b005cc35d26234712852205d0bafac5156a5

move formatting to constraints

src/document/constraint/constraint_point_line_distance.cpp
@@ -6,6 +6,7 @@
 #include "util/json_util.hpp"
 #include "util/glm_util.hpp"
 #include "constraintt_impl.hpp"
+#include <format>
 
 namespace dune3d {
 ConstraintPointLineDistance::ConstraintPointLineDistance(const UUID &uu) : Base(uu)
@@ -77,6 +78,11 @@ double ConstraintPointLineDistance::get_display_datum(const Document &doc) const
         return measure_distance(doc);
     else
         return m_distance;
+}
+
+std::string ConstraintPointLineDistance::format_datum(double datum) const
+{
+    return std::format("{:.3f}", datum);
 }
 
 } // namespace dune3d

src/document/constraint/constraint_point_plane_distance.cpp
@@ -6,6 +6,7 @@
 #include "util/json_util.hpp"
 #include "util/glm_util.hpp"
 #include "constraintt_impl.hpp"
+#include <format>
 
 namespace dune3d {
 ConstraintPointPlaneDistance::ConstraintPointPlaneDistance(const UUID &uu) : Base(uu)
@@ -80,6 +81,11 @@ double ConstraintPointPlaneDistance::get_display_datum(const Document &doc) cons
         return measure_distance(doc);
     else
         return m_distance;
+}
+
+std::string ConstraintPointPlaneDistance::format_datum(double datum) const
+{
+    return std::format("{:.3f}", datum);
 }
 
 } // namespace dune3d

src/document/constraint/constraint_angle.cpp
@@ -5,6 +5,7 @@
 #include "util/glm_util.hpp"
 #include "constraint_visitor.hpp"
 #include "document/document.hpp"
+#include <format>
 
 namespace dune3d {
 ConstraintAngleBase::ConstraintAngleBase(const UUID &uu) : Constraint(uu)
@@ -132,6 +133,11 @@ double ConstraintLinesAngle::get_display_datum(const Document &doc) const
         return measure_datum(doc);
     else
         return m_angle;
+}
+
+std::string ConstraintLinesAngle::format_datum(double datum) const
+{
+    return std::format("{:.1f}°", datum);
 }
 
 } // namespace dune3d

src/document/constraint/constraint_angle.hpp
@@ -107,6 +107,8 @@ public:
         return DatumUnit::DEGREE;
     }
 
+    std::string format_datum(double datum) const override;
+
     std::pair<double, double> get_datum_range() const override
     {
         return {0, 360};

src/document/constraint/constraint_diameter_radius.cpp
@@ -9,6 +9,7 @@
 #include "util/json_util.hpp"
 #include "util/glm_util.hpp"
 #include "constraint_visitor.hpp"
+#include <format>
 
 namespace dune3d {
 ConstraintDiameterRadius::ConstraintDiameterRadius(const UUID &uu) : Constraint(uu)
@@ -101,6 +102,11 @@ double ConstraintDiameterRadius::get_display_datum(const Document &doc) const
         return measure_distance(doc);
     else
         return m_distance;
+}
+
+std::string ConstraintDiameterRadius::format_datum(double datum) const
+{
+    return std::format("{:.3f}", datum);
 }
 
 } // namespace dune3d

src/document/constraint/constraint_diameter_radius.hpp
@@ -82,6 +82,8 @@ public:
 
     double get_display_datum(const Document &doc) const override;
 
+    std::string format_datum(double datum) const override;
+
     void accept(ConstraintVisitor &visitor) const override;
 
     bool replace_point(const EntityAndPoint &old_point, const EntityAndPoint &new_point) override;

src/document/constraint/constraint_point_distance.cpp
@@ -7,6 +7,7 @@
 #include "util/json_util.hpp"
 #include "util/glm_util.hpp"
 #include "constraint_visitor.hpp"
+#include <format>
 
 namespace dune3d {
 ConstraintPointDistanceBase::ConstraintPointDistanceBase(const UUID &uu) : Constraint(uu)
@@ -91,6 +92,11 @@ double ConstraintPointDistanceBase::get_display_datum(const Document &doc) const
         return measure_distance(doc);
     else
         return m_distance;
+}
+
+std::string ConstraintPointDistanceBase::format_datum(double datum) const
+{
+    return std::format("{:.3f}", datum);
 }
 
 double ConstraintPointDistanceBase::measure_datum(const Document &doc) const

src/document/constraint/constraint_point_distance.hpp
@@ -73,6 +73,8 @@ public:
         return DatumUnit::MM;
     }
 
+    std::string format_datum(double datum) const override;
+
     const UUID &get_workplane(const Document &doc) const override
     {
         return m_wrkpl;

commit eeefc23cdb2e61a230ac1627ce47e1f8bf5ce75d

don't show sign for some distance constraints
got broken in 7880b005cc35d26234712852205d0bafac5156a5

src/document/constraint/constraint_point_line_distance.cpp
@@ -75,9 +75,9 @@ double ConstraintPointLineDistance::measure_datum(const Document &doc) const
 double ConstraintPointLineDistance::get_display_datum(const Document &doc) const
 {
     if (m_measurement)
-        return measure_distance(doc);
+        return std::abs(measure_distance(doc));
     else
-        return m_distance;
+        return std::abs(m_distance);
 }
 
 std::string ConstraintPointLineDistance::format_datum(double datum) const

src/document/constraint/constraint_point_plane_distance.cpp
@@ -78,9 +78,9 @@ double ConstraintPointPlaneDistance::measure_datum(const Document &doc) const
 double ConstraintPointPlaneDistance::get_display_datum(const Document &doc) const
 {
     if (m_measurement)
-        return measure_distance(doc);
+        return std::abs(measure_distance(doc));
     else
-        return m_distance;
+        return std::abs(m_distance);
 }
 
 std::string ConstraintPointPlaneDistance::format_datum(double datum) const