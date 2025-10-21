4ebbb706910f432ee588f47ae887c0b15a2a653f
Lukas K. <lu@0x83.eu>
2025-10-22 01:11:38 +0200

    tool import step: add rotation actions
    changelog: Enhancemens/Editor: allow rotating step model in import tool

--- src/core/tools/tool_import_step.cpp
+++ src/core/tools/tool_import_step.cpp
@@ -1,11 +1,14 @@
 #include "tool_import_step.hpp"
 #include "document/document.hpp"
 #include "document/entity/entity_step.hpp"
+#include "document/constraint/constraint_lock_rotation.hpp"
 #include "editor/editor_interface.hpp"
 #include "dialogs/dialogs.hpp"
 #include "import_step/step_import_manager.hpp"
 #include <gtkmm.h>
 #include "util/fs_util.hpp"
+#include "util/glm_util.hpp"
+#include "util/action_label.hpp"
 #include "tool_common_impl.hpp"
 #include "core/tool_data_path.hpp"
 
@@ -60,25 +63,97 @@ ToolResponse ToolImportSTEP::begin(const ToolArgs &args)
 }
 
 
+void ToolImportSTEP::update_tip()
+{
+    std::string tip = "Orientation: " + quat_to_string(m_step->m_normal);
+
+    std::vector<ConstraintType> constraint_icons;
+
+    if (m_lock_rotation)
+        constraint_icons.push_back(ConstraintType::LOCK_ROTATION);
+
+    std::vector<ActionLabelInfo> actions;
+    actions.reserve(9);
+    actions.emplace_back(InToolActionID::LMB, "place");
+
+    actions.emplace_back(InToolActionID::RMB, "cancel");
+
+    if (m_lock_rotation)
+        actions.emplace_back(InToolActionID::TOGGLE_LOCK_ROTATION_CONSTRAINT, "unlock rotation");
+    else
+        actions.emplace_back(InToolActionID::TOGGLE_LOCK_ROTATION_CONSTRAINT, "lock rotation");
+
+    actions.emplace_back(InToolActionID::ROTATE_X, InToolActionID::ROTATE_Y, InToolActionID::ROTATE_Z, "rotate");
+
+    m_intf.set_constraint_icons(m_intf.get_cursor_pos(), {NAN, NAN, NAN}, constraint_icons);
+
+    m_intf.tool_bar_set_actions(actions);
+    m_intf.tool_bar_set_tool_tip(tip);
+}
+
 ToolResponse ToolImportSTEP::update(const ToolArgs &args)
 {
-    if (args.type == ToolEventType::DATA) {
+    if (args.type == ToolEventType::MOVE) {
+        if (m_step)
+            m_step->m_origin = m_intf.get_cursor_pos();
+
+        set_first_update_group_current();
+    }
+    else if (args.type == ToolEventType::DATA) {
         if (auto data = dynamic_cast<const ToolDataPath *>(args.data.get())) {
             if (data->path != std::filesystem::path{}) {
-                auto &step = add_entity<EntitySTEP>();
+                m_step = &add_entity<EntitySTEP>();
                 auto dir = m_core.get_current_document_directory();
                 if (auto rel = get_relative_filename(data->path, dir))
-                    step.m_path = *rel;
+                    m_step->m_path = *rel;
                 else
-                    step.m_path = data->path;
-                step.update_imported(dir);
-                return ToolResponse::commit();
+                    m_step->m_path = data->path;
+                m_step->update_imported(dir);
+                m_step->m_origin = m_intf.get_cursor_pos();
             }
             else {
                 return ToolResponse::end();
             }
         }
     }
+
+    else if (args.type == ToolEventType::ACTION) {
+        switch (args.action) {
+        case InToolActionID::LMB:
+            if (m_lock_rotation) {
+                auto &constraint = add_constraint<ConstraintLockRotation>();
+                constraint.m_entity = m_step->m_uuid;
+            }
+            return ToolResponse::commit();
+            break;
+
+        case InToolActionID::RMB:
+        case InToolActionID::CANCEL:
+            return ToolResponse::revert();
+
+        case InToolActionID::TOGGLE_LOCK_ROTATION_CONSTRAINT: {
+            m_lock_rotation = !m_lock_rotation;
+        } break;
+
+        case InToolActionID::ROTATE_X:
+        case InToolActionID::ROTATE_Y:
+        case InToolActionID::ROTATE_Z: {
+            const auto axis = static_cast<int>(args.action) - static_cast<int>(InToolActionID::ROTATE_X);
+            glm::dvec3 a = {0, 0, 0};
+            a[axis] = 1;
+            const auto q = glm::angleAxis(M_PI / 2, a);
+
+            m_lock_rotation = true;
+            m_step->m_normal = q * m_step->m_normal;
+
+        } break;
+
+        default:;
+        }
+    }
+
+    update_tip();
+
     return ToolResponse();
 }
 } // namespace dune3d
--- src/core/tools/tool_import_step.hpp
+++ src/core/tools/tool_import_step.hpp
@@ -1,8 +1,10 @@
 #include "tool_common.hpp"
+#include "in_tool_action/in_tool_action.hpp"
 
 namespace dune3d {
 
 class ConstraintPointDistanceBase;
+class EntitySTEP;
 
 class ToolImportSTEP : public ToolCommon {
 public:
@@ -12,7 +14,19 @@ public:
     ToolResponse begin(const ToolArgs &args) override;
     ToolResponse update(const ToolArgs &args) override;
 
+    std::set<InToolActionID> get_actions() const override
+    {
+        using I = InToolActionID;
+        return {
+                I::LMB, I::CANCEL, I::RMB, I::TOGGLE_LOCK_ROTATION_CONSTRAINT, I::ROTATE_X, I::ROTATE_Y, I::ROTATE_Z,
+        };
+    }
 
 private:
+    EntitySTEP *m_step = nullptr;
+
+    bool m_lock_rotation = false;
+
+    void update_tip();
 };
 } // namespace dune3d
